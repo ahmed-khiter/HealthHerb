3a87326b193d02af210a9bd02a3b562e0a524112
ahmed-khiter <ahmed.khaled.atyia@gmail.com>
2020-11-24 17:45:48 +0200

    Finished website

--- HealthHerb/Startup.cs
+++ HealthHerb/Startup.cs
@@ -93,7 +93,7 @@ namespace HealthHerb
             {
                 app.UseExceptionHandler("/Home/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                //app.UseHsts();
+                app.UseHsts();
             }
 
             Seed.SeedUser(userManager, roleManager);
@@ -101,7 +101,7 @@ namespace HealthHerb
             var payment = paymentSettingCrud.GetById("PaymentSetting").Result;
             StripeConfiguration.ApiKey = payment.SecretKey;
 
-            //app.UseHttpsRedirection();
+            app.UseHttpsRedirection();
 
             app.UseStaticFiles();
 
